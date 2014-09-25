ef6c3454d0a8ab56bf16ab871308af00a7136c16
MohamedBassem <medoox240@gmail.com>
2014-09-25 13:52:39 +0300

    Adding new params to user

--- app/app/models/User.php
+++ app/app/models/User.php
@@ -1,52 +1,64 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: mohamedfarghal
- * Date: 9/20/14
- * Time: 10:47 AM
- * To change this template use File | Settings | File Templates.
- */
 
 class User extends BaseModel {
 
-    function __construct() {
-        parent::__construct("users");
-    }
+  function __construct() {
+    parent::__construct("users");
+  }
 
-    public function setEmail($email)
-    {
-        $this->set("email", $email);
-    }
+  public function getId()
+  {
+    return $this->get("id");
+  }
 
-    public function setName($name)
-    {
-        $this->set("name", $name);
-    }
+  public function setEmail($email)
+  {
+    $this->set("email", $email);
+  }
 
-    public function setPassword($password)
-    {
-        $this->set("password", $password);
-    }
+  public function getEmail()
+  {
+    return $this->get("email");
+  }
 
-    public function getPassword()
-    {
-        return $this->get("password");
-    }
+  public function setFirstName($name)
+  {
+    $this->set("firstName", $name);
+  }
 
-    public function getName()
-    {
-        return $this->get("name");
-    }
+  public function getFirstName()
+  {
+    return $this->get("firstName");
+  }
 
-    public function getId()
-    {
-        return $this->get("id");
-    }
+  public function setLastName($name)
+  {
+    $this->set("lastName", $name);
+  }    
 
+  public function getLastName()
+  {
+    return $this->get("lastName");
+  }
 
-    public function getEmail()
-    {
-        return $this->get("email");
-    }
+  public function setPassword($password)
+  {
+    $this->set("password", $password);
+  }
+
+  public function getPassword()
+  {
+    return $this->get("password");
+  }
+
+  public function setProfilePicturePath($profilePicturePath)
+  {
+    return $this->set("profilePicturePath",$profilePicturePath);
+  }
+
+  public function getProfilePicturePath()
+  {
+    return $this->get("profilePicturePath");
+  }
 
 }
