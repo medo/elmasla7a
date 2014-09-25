11ef1f2f129d42b989a78cf7af8a00de239f7098
Alaa Maher <almgohar@gmail.com>
2014-09-25 12:40:04 +0300

    Add registerAction + Fix redirections

--- app/app/controllers/CartController.php
+++ app/app/controllers/CartController.php
@@ -3,35 +3,40 @@
 class CartController extends BaseController{
 
   function indexAction(int $userId) {
-  	$cartItems = cart::findById(array("userId" => $userId, "bought" => false));
+  	$cartItems = Cart::findById(array("userId" => $userId, "bought" => false));
   	return $this->render("index.html.haml", array("cartItems" => $cartItems, "userId" => $userId));
 
   }
 
   function buyAction(int $userId, int $itemId) {
-  	$item = items::findById(array("id" => $itemId));
+  	$item = Item::findById(array("id" => $itemId));
   	if($item->getQuantity() != 0) {
-    $cart = new Cart();
+    $cart = new cart();
     $cart->setUserId($userId);
     $cart->setItemId($itemId);
     $cart->setBought(false); 
-    $cart->save();   
+    $cart->save();  
+    return $this->redirect(SiteController, index); 
   	}
   }
 
   function checkoutAction(int $userId) {
-  	$cartItems = cart::findById(array("userId" => $userId));
-  	foreach ($cartItems as $item) {
-      if(!$item->getBought()) {
-        $item->setBought(true);
+  	$cartItems = Cart::findById(array("userId" => $userId));
+  	foreach ($cartItems as $cartItem) {
+      if(!$cartItem->getBought()) {
+        $cartItem->setBought(true);
+        $itemId = $cartItem->getItemId();
+        $item  = Item::findById(array("id" => $itemId));
         $item->setQuantity($item->getQuantity() - 1);
         $item->save();
+        $cartItem->save();
       }
     }
+    return $this->redirect(CartController, index);
   }
 
   function historyAction(int $userId) {
-    $boughtItems = cart::findById(array("userId" => $userId, "bought" => true));
+    $boughtItems = Cart::findById(array("userId" => $userId, "bought" => true));
     return $this->render("history.html.haml", array("boughtItems" => $boughtItems, "userId" => $userId));
   }
 }
--- app/app/controllers/UserController.php
+++ app/app/controllers/UserController.php
@@ -2,15 +2,22 @@
 
 class UserController extends BaseController{
 
-  function registerAction(String $firstName, String $lastName, String $email, String $password) {
-  	
+  function registerAction(String $name, String $email, String $password) {
+  	$user = new User();
+    $user->setName($name);
+    $user->setEmail($email);
+    $user->setPassword(hash("md5",$password,false));
+    $user->save();
+    return $this->redirect(UserController, login);
   }
 
   function loginAction(String $email, String $password) {
-
+    
+    return $this->redirect(SiteController, index);
   }
 
   function logoutAction(int $userId) {
 
+    return $this->redirect(SiteController, index);
   }
 }
