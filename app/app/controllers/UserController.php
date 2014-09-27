4b12e26f20c5736c83549f6b5dd034f0f2a29f48
Alaa Maher <almgohar@gmail.com>
2014-09-27 03:07:06 +0300

    Show error messages

--- app/app/controllers/UserController.php
+++ app/app/controllers/UserController.php
@@ -2,24 +2,6 @@
 
 class UserController extends BaseController{
 
-  function registerAction($params) {
-    $firstName = $params["firstName"];
-    $lastName = $params["lastName"];
-    $email = $params["email"];
-    $password = $params["password"];
-    $profilePicturePath = $params["profilePicturePath"];
-  	$user = new User();
-    $user->firstName = $firstName;
-    $user->lastName = $lastName;
-    $user->email = $email;
-    $user->password = md5($password);
-    $user->profilePicturePath = $profilePicturePath;
-    $user->save();
-    $this->signInUser($user->Id);
-    
-    return $this->redirect("Site", "index");
-  }
-
   function editAction($params) {
     $user = $this->signedInUser();
     $firstName = $user->firstName;
@@ -45,23 +27,9 @@ class UserController extends BaseController{
 
   }
 
-  function loginAction($params) {
-    $email = $params["email"];
-    $password = md5($params["password"]);
-    $user = User::model()->findOne(array("email" => $email, "password" => $password));
-    if($user!= null){
-      $this->signInUser($user->id);
-     return $this->redirect("Site", "index");
-    } else {
-      $message = true;
-      return $this->redirect("Site", "index");
-    }
-    
-   
-  }
-
   function logoutAction($params) {
     $_SESSION['userId'] = null;
     return $this->redirect("Site", "index");
   }
+
 }
